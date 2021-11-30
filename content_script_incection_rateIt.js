fc7ea364fcd75fbdffd366aa4e1f54ec28f750bd
TimonGaertner <timon.ben.gaertner@gmail.com>
2021-11-30 19:22:31 +0100

    add youtube support

---
+++ content_script_incection_rateIt.js
@@ -0,0 +1,74 @@
+let currentUrl = (window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search).toLowerCase();
+const server = "https://rateit.timon-gaertner.ga/"
+let liked = false;
+let disliked = false;
+
+function sendRating(rating) {
+    $.ajax({
+        url: server+"post/rating",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({url: currentUrl, rating: rating})
+    });
+}
+async function fillDislikes(htmlDislikeSelector) {
+    currentUrl = currentUrl[0].url;
+    rating = await $.ajax({
+        url: server+"get/rating",
+        type: "get",
+        data: {
+            url: currentUrl,
+        },
+        error: function (xhr) {},
+    })
+    dislikes = rating.dislikes
+
+    userRating = rating.userRating;
+    if (userRating == 1) {
+        liked = true;
+    } else if (userRating == -1) {
+        disliked = true;
+    }
+    htmlDislikeSelector.text(dislikes);
+}
+async function fillLikes(htmlLikeSelector) {
+    currentUrl = currentUrl[0].url;
+    rating = await $.ajax({
+        url: server+"get/rating",
+        type: "get",
+        data: {
+            url: currentUrl,
+        },
+        error: function (xhr) {},
+    })
+    likes = rating.likes
+
+    userRating = rating.userRating;
+    if (userRating == 1) {
+        liked = true;
+    } else if (userRating == -1) {
+        disliked = true;
+    }
+    htmlLikeSelector.text(likes);
+}
+
+
+
+
+
+
+
+function assignDislikeButton(htmlLikeSelector) {
+    htmlLikeSelector.click(function () {
+        if (disliked) {sendRating(0); liked=false;}
+        else{
+        sendRating(-1);
+    }
+})}
+function assignLikeButton(htmlDislikeSelector) {
+    htmlDislikeSelector.click(function () {
+        if (liked) {sendRating(0); liked=false;}
+        else{
+        sendRating(1);
+    }
+})}
