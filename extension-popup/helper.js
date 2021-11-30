ef4f4e7507a9f35a768110e6729142e786eb16d2
TimonGaertner <timon.ben.gaertner@gmail.com>
2021-11-30 21:06:04 +0100

    logout, if post error

--- extension-popup/helper.js
+++ extension-popup/helper.js
@@ -1,36 +1,59 @@
-function verify(token){
+function verify(token) {
     $.ajax({
-            url: server+"verify",
-            type: "post",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({token: token})
+        url: server + "verify",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ token: token }),
     });
 }
-function login(token){
+function login(token) {
     $.ajax({
-            url: server+"login",
-            type: "post",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({token: token})
+        url: server + "login",
+        type: "post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ token: token }),
     });
 }
 function sendRating(rating) {
     $.ajax({
-        url: server+"post/rating",
+        url: server + "post/rating",
         type: "post",
         contentType: "application/json; charset=utf-8",
-        data: JSON.stringify({url: currentUrl, rating: rating})
+        data: JSON.stringify({ url: currentUrl, rating: rating }),
+        error: function (xhr) {
+            chrome.storage.sync.remove("loggedIn");
+            $("body .wrapper").html(buildLoginHtml());
+            $("#googleLogin").click(function () {
+                chrome.runtime.sendMessage(
+                    { msg: "login" },
+                    function (response) {
+                        if (response == "success") {
+                            $("body .wrapper").html(buildIndexHtml());
+                            getAndFillRating();
+                            $("#likeButton").click(function () {
+                                onLike();
+                            });
+
+                            $("#dislikeButton").click(function () {
+                                onDislike();
+                            });
+                        }
+                    }
+                );
+                console.log("login");
+            });
+        },
     });
 }
 async function getRating(url) {
-    return (await $.ajax({
-        url: server+"get/rating",
+    return await $.ajax({
+        url: server + "get/rating",
         type: "get",
         data: {
             url: url,
         },
         error: function (xhr) {},
-    }))
+    });
 }
 
 function onLike() {
@@ -48,7 +71,7 @@ function onLike() {
         fillLikeCount();
     } else {
         liked = false;
-        sendRating(0)
+        sendRating(0);
         $("#like .filledActive").removeClass("filledActive");
         likes--;
         fillLikeCount();
@@ -70,7 +93,7 @@ function onDislike() {
         fillDislikeCount();
     } else {
         disliked = false;
-        sendRating(0)
+        sendRating(0);
         $("#dislike .filledActive").removeClass("filledActive");
         dislikes--;
         fillDislikeCount();
