d9fb9a902e02b09dfc7cc5185dcac929058bcd74
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2021-12-02 00:47:13 +0100

    Delete youtubeVideo.js

--- youtubeVideo.js
+++
@@ -1,7 +0,0 @@
-fillDislikes($('ytd-toggle-button-renderer a yt-formatted-string').eq(1))
-likes=$('ytd-toggle-button-renderer a yt-formatted-string').eq(0)
-if (!(/^\d+\.\d+$/.test(likes.text()))) {
-    fillLikes(likes)
-}
-assignDislikeButton($('ytd-toggle-button-renderer .yt-simple-endpoint').eq(1))
-assignLikeButton($('ytd-toggle-button-renderer .yt-simple-endpoint').eq(0))
