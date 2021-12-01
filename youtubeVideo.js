db6d953715747d66824ec05c990638e90a6d9a63
1NO0B <timon.ben.gaertner@gmail.com>
2021-12-01 09:07:12 +0100

    fix - youtube likes with points dont get replaced

--- youtubeVideo.js
+++ youtubeVideo.js
@@ -1,6 +1,6 @@
 fillDislikes($('ytd-toggle-button-renderer a yt-formatted-string').eq(1))
 likes=$('ytd-toggle-button-renderer a yt-formatted-string').eq(0)
-if (!(/^\d+$/.test(likes.text()))) {
+if (!(/^\d+\.\d+$/.test(likes.text()))) {
     fillLikes(likes)
 }
 assignDislikeButton($('ytd-toggle-button-renderer .yt-simple-endpoint').eq(1))
